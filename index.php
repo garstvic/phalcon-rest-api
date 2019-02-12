796180c04ffce0e9b00486086353061941a75a1c
Victor Garstukov <garstvic@gmail.com>
2019-02-12 12:22:12 +0000

    Added Create, Update and Delete Routes

--- index.php
+++ index.php
@@ -128,19 +128,202 @@ $app->get(
 );
 
 // Adds a new cars
-$app->post('/api/cars', function() use ($app) {
-    
-});
+$app->post(
+    '/api/cars', 
+    function() use ($app) 
+    {
+        $phql = '
+            INSERT INTO Cars (
+                owner_name, 
+                reg_date, 
+                license_plate_on, 
+                engine_no, 
+                tax_payment, 
+                car_model, 
+                car_model_year, 
+                seating_capacity, 
+                horse_power) 
+            VALUES (
+                :owner_name:, 
+                :reg_date:, 
+                :license_plate_on:, 
+                :engine_no:, 
+                :tax_payment:, 
+                :car_model:, 
+                :car_model_year:, 
+                :seating_capacity:
+            )';
+            
+        $car = $app->request->getJsonRawBody();
+        $values = array(
+            'id'                => $car->id,
+            'owner_date'        => $car->owner_name,
+            'reg_date'          => $car->reg_date,
+            'license_plate_no'  => $car->license_plate_no,
+            'engine_no'         => $car->engine_no,
+            'tax_payment'       => $car->tax_payment,
+            'car_model'         => $car->car_model,
+            'seating_capacity'  => $car->seating_capacity,
+            'horse_power'       => $car->horse_power            
+        );
+        
+        $result = $app->modelsManager->executeQuery($phql, $values);
+        
+        $response = new Response();
+        
+        if ($result->success() == TRUE)
+        {
+            $response->setStatusCode(201, 'Created');
+            
+            $car->id = $results->getModel()->id;
+            
+            $response->setJsonContent(
+                array(
+                    'status'    =>  'OK',
+                    'data'      =>  $car
+                )  
+            );
+        }
+        else
+        {
+            $response->setStatusCode(409, 'Conflict');
+            
+            $errors = array();
+            
+            foreach($results->getMessages() as $message)
+            {
+                $errors[] = $message->getMessage();
+            }
+            
+            $response->setJsonContent(
+                array(
+                    'status'    =>  'ERROR',
+                    'message'   =>  $errors
+                )  
+            );
+        }
+        
+        return $response;
+    }
+);
 
 // Updates car based on primary key ($id)
-$app->put('/api/cars/{id:[0-9]+}', function($id) use ($app) {
-    
-});
+$app->put(
+    '/api/cars/{id:[0-9]+}', 
+    function($id) use ($app)
+    {
+        $phql = 'UPDATES Cars SET
+                    owner_name = :owner_name:,
+                    reg_date = :reg_date,
+                    license_plate_no = :license_plate_no,
+                    engine_no = :engine_no:,
+                    tax_payment = :tax_payment:,
+                    car_model = :car_model:,
+                    car_model_year = :car_model_year:,
+                    seating_capacity = :seating_capacity:,
+                    horse_power = :horse_power:
+                WHERE id = :id:';
+        
+        $updatedCarValues = $app->request->getJsonRawBody();
+        
+        $values = array(
+            'id'                => $id,
+            'owner_date'        => $updatedCarValues->owner_name,
+            'reg_date'          => $updatedCarValues->reg_date,
+            'license_plate_no'  => $updatedCarValues->license_plate_no,
+            'engine_no'         => $updatedCarValues->engine_no,
+            'tax_payment'       => $updatedCarValues->tax_payment,
+            'car_model'         => $updatedCarValues->car_model,
+            'seating_capacity'  => $updatedCarValues->seating_capacity,
+            'horse_power'       => $updatedCarValues->horse_power           
+        );
+        
+        $result = $app->modelsManager->executeQuery($phql, $values);
+        
+        $response = new Response();
+        
+        if ($result->success() == TRUE)
+        {
+            $response->setStatusCode(200, 'OK');
+            
+            $car = $result->getModel()->id;
+            
+            $response->setJsonContent(
+                array(
+                    'status'    =>  'OK'    
+                )  
+            );
+        }
+        else
+        {
+            $response->setStatusCode(409, 'Conflict');
+            
+            $errors = array();
+            
+            foreach($result->getMessages() as $message)
+            {
+                $errors[] = $message->getMessage();
+            }
+            
+            $response->setJsonContent(
+                array(
+                    'status'    =>  'ERROR',
+                    'message'   =>  $errors
+                )  
+            );
+        }
+        
+        return $response;    
+    }
+);
 
 // Deletes car based on primary key ($id)
-$app->delete('/api/cars/{id:[0-9]+}', function($id) use ($app) {
-    
-});
+$app->delete(
+    '/api/cars/{id:[0-9]+}', 
+    function($id) use ($app)
+    {
+        $phql = 'DELETE FROM Cars WHERE id = :id:';
+        
+        $values = array(
+            'id'    =>  $id  
+        );
+        
+        $result = $app->modelsManager->executeQuery($phql, $values);
+        
+        $response = new Response();
+        
+        if ($result->success() == TRUE)
+        {
+            $response->setStatusCode(200, 'OK');
+            
+            $response->setJsonContent(
+                array(
+                    'status'    =>  'OK'    
+                )  
+            );
+        }
+        else
+        {
+            $response->setStatusCode(409, 'Conflict');
+            
+            $errors = array();
+            
+            foreach($result->getMessage() as $message)
+            {
+                $errors[] = $message->getMessage();
+            }
+            
+            $response->setJsonContent(
+                array(
+                    'status'    =>  'ERROR',
+                    'message'   =>  $errors
+                )  
+            );
+        }
+        
+        return $response;
+    }
+);
 
 $app->notFound(function() use ($app) {
     $app->response->setStatusCode(404, "Not Found")->sendHeaders();
